dc53069af248d60e06fcd2dab9d8dec1f9a774e1
Actions <actions@github.com>
2026-02-01 10:32:56 +0000

    Update config

--- config.js
+++ config.js
@@ -124,6 +124,31 @@ window.shopConfigs = {
         }
     ]
 }
+,
+    'l_ml3lqjmvr9dt': {
+    "name": "李家凉菜",
+    "themeColor": "#e63946",
+    "logo": "images/logo.png",
+    "qrcode": "https://slonbvmhsxqgpoodwazj.supabase.co/storage/v1/object/public/shop-qrcodes/l_ml3lqjmvr9dt_wechat_qrcode_1769941948136.png",
+    "dishes": [
+        {
+            "id": 1,
+            "name": "招牌炸酱面",
+            "price": 15,
+            "emoji": "🍜",
+            "category": "主食",
+            "tags": []
+        },
+        {
+            "id": 2,
+            "name": "麻辣烫套餐",
+            "price": 18,
+            "emoji": "🥘",
+            "category": "主食",
+            "tags": []
+        }
+    ]
+}
 };
 
 // 获取当前商家ID
